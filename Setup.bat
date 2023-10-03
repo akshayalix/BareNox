ef2e3b6833674fe4dd8f00aed271f8c86f37ed4e
Alix <akshayalix12@gmail.com>
2023-10-03 23:47:53 +0530

    initial

--- Setup.bat
+++ Setup.bat
@@ -1,43 +1,44 @@
 @echo off
 
-type "%CD%\art\art.txt"
+::REM Delay in milliseconds (1000 milliseconds = 1 second)
+::set "delay=1000"
 
+::ping -n 2 127.0.0.1 > nul
 
-
-pause
-echo Downloading Files Using Curl...
+echo -^> Downloading Files Using Curl...
+echo:
 "%cd%\Tools\Curl\curl.exe"  -o ./NoxPlayerSetup.exe -J -L bignox.com/en/download/fullPackage?beta
-echo Download Complete.
+echo -^> Download Complete....
+echo:
 
-pause
-echo Creating Directory Structure...
+echo -^> Creating Directory Structure...
+echo:
 mkdir "%cd%\NoxPlayer"
-echo Complete.
+echo -^> Completed......
+echo:
 
-pause
-echo Extracting Files...
+echo -^> Extracting Files...
 "%cd%\Tools\7-Zip\App\7-Zip\7za.exe" x NoxPlayerSetup.exe -oNoxPlayer\Nox 
-echo Extracting Complete.
+echo:
+echo -^> Extraction Complete....
+echo:
 
-pause
-echo Going To Extract Some Required Files...
-"%cd%\Tools\7-Zip\App\7-Zip\7za.exe" x "%CD%\assets\Bignox.7z" -oNoxPlayer\Bignox 
-echo Extracting Complete.
+::echo ->^ Going To Extract Some Required Files...
+::"%cd%\Tools\7-Zip\App\7-Zip\7za.exe" x "%CD%\assets\Bignox.7z" -o NoxPlayer\Bignox 
+::echo ->^ Extracting Complete....
 
-pause 
-echo Going To Copy Run Scripts...
-copy "%cd%\assets\SecretRunScripts\*" "%cd%\NoxPlayer"
-echo Copy Complete.
+echo -^> Removing Temp Files...
+echo:
+del NoxPlayerSetup.exe
+echo -^> Remove Complete...
+echo:
 
-type "%CD%\art\thanks.txt"
+echo -^> Going To Copy Run Scripts...
+copy "%cd%\Scripts\*" "%cd%\NoxPlayer"
+echo:
+echo -^> Copy Complete...
+echo:
 
 
-pause
-type "%CD%\art\Credits.txt"
 
-
-pause
-
-echo Copy The NoxPlayer Folder To Where Ever You Like, Like A USB-Stick 
-echo To Run NoxPlayer Go To The NoxPlayer Folder And Run RunNox.bat
 pause
